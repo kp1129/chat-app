dfa0dfbb9c79a6c8b764ca81af10a3dd5d1b47ce
kp1129 <katya.p.1129@gmail.com>
2020-11-30 23:35:10 -0700

    create react app with redux template, set up main components, create sidebar header

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import IMessage from './IMessage';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's get this party started</h1>
+      <IMessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/IMessage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './IMessage.css';
+
+import Sidebar from './Sidebar';
+
+function IMessage() {
+    return (
+        <div className="imessage">
+            <Sidebar />
+            
+        </div>
+    )
+}
+
+export default IMessage
